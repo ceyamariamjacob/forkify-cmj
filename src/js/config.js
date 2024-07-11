b1fad0d6f913d6fdadd29f693104879b23f0e69f
ceyamariamjacob <122643049+ceyamariamjacob@users.noreply.github.com>
2024-07-11 23:16:21 +0300

    new commit

--- src/js/config.js
+++ src/js/config.js
@@ -3,4 +3,4 @@ export const API_URL='https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC=10;
 export const RES_PER_PAGE=10;
 export const KEY='f5fdea0c-adb0-4034-aa01-98c8058a4818';
-export const MODAL_CLOSE_sEC=2.5;
+export const MODAL_CLOSE_sEC=2.5;  
